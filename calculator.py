d1a94b956ec2722e150a96465086c0427e34e199
TanmayShaurya04 <tanmayshaurya04@gmail.com>
2026-06-02 11:44:49 +0530

    learning

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a=17
+b=28
+print("sum of ", a, " and ", b, " is ", a+b)
+print("difference of ", a, " and ", b, " is ", a-b)
+print("product of ", a, " and ", b, " is ", a*b)
+print("division of ", a, " and ", b, " is ", a/b)
+print("exponentiation of ", a, " and ", b, " is ", a**b)
+print("floor division of ", a, " and ", b, " is ", a//b)
+print("modulus of ", a, " and ", b, " is ", a%b)
---
+++ main.py
@@ -0,0 +1,8 @@
+print("hello world")
+print(5)
+print("hello tanmay how are you","are you fine?");
+print(3+4+3+5+7+8+9+6+5+4+3+2+1)
+print("the sum of 3+4+3+5+7+8+9+6+5+4+3+2+1 is\n",3+4+3+5+7+8+9+6+5+4+3+2+1)
+# this is known as comment which does not execute
+# Author : Tanmay 
+print("he is a \"good boy\" and a \"good student\"")
