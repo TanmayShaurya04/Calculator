64794403cc2f4659150044db878801c5481c364f
TanmayShaurya04 <tanmayshaurya04@gmail.com>
2026-06-02 12:13:41 +0530

    only calculator file

--- operators.py
+++
@@ -1,8 +0,0 @@
-print(5+6) #addition
-print(10-3) #subtraction
-print(4*7) #multiplication
-print(15/4) #division
-print(2**3) #exponentiation
-print(15//4) #floor division
-print(15%3) #modulus 
- 
