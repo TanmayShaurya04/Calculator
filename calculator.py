6f658b9df45e7a4f72b8cb58f7730269728e4f15
TanmayShaurya04 <tanmayshaurya04@gmail.com>
2026-06-02 12:06:45 +0530

    calculator

--- calculator.py
+++
@@ -1,9 +0,0 @@
-a=17
-b=28
-print("sum of ", a, " and ", b, " is ", a+b)
-print("difference of ", a, " and ", b, " is ", a-b)
-print("product of ", a, " and ", b, " is ", a*b)
-print("division of ", a, " and ", b, " is ", a/b)
-print("exponentiation of ", a, " and ", b, " is ", a**b)
-print("floor division of ", a, " and ", b, " is ", a//b)
-print("modulus of ", a, " and ", b, " is ", a%b)
